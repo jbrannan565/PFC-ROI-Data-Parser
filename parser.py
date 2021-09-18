6a522c75c9ff7d93074d93912efe7e7527b51348
Jared Brannan <jared.brannan@wsu.edu>
2021-09-18 14:47:00 -0700

    change to take arguments and allow for full directories to be passed

--- parser.py
+++ parser.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # coding: utf-8
 '''
 Description:
-    Take an input file 'log.txt' and format into a csv file with columns x,y and cnum (the last of which represents the coordinate number e.g. 'x123' or 'y451 in data.
-    Saves the csv as 'data_out.csv'
+    Single file mode:
+        Take an input file name passed after --in-file and format into a csv file with 
+        columns x,y and cnum (the last of which represents the coordinate number, 
+        e.g. 'x123' or 'y451' in data). Saves the csv as name passed after --out-file.
+
+    Multi-file mode:
+        Take an input directory (folder) name passed after --in-dir and format each txt file
+        into a csv file with columns x,y and cnum (the last of which represents the coordinate number, 
+        e.g. 'x123' or 'y451 in data). Saves the results in the directory passed after --out-dir
+        as a csv file with the name of the file processed with .csv instead of .txt 
+        (e.g. if the file was named FILE.txt, the csv file will be named FILE.csv).
+
+Usage:
+    ./parser.py --in-file IN_FILE.txt --out-file OUT_FILE.csv
+    ./parser.py --in-dir IN_DIR_PATH --out-dir OUT_DIR_PATH
 
 Dependencies:
     -python>=3.8
@@ -11,6 +24,8 @@ Dependencies:
 '''
 
 import pandas
+import argparse
+import os
 
 def log_df_to_xy_df(df):
     """
@@ -46,19 +61,62 @@ def log_df_to_xy_df(df):
     return final_data
 
 
+def read_roi_file_parse_and_save(in_file_name, out_file_name):
+    """
+    Processes contents of in_file_name as ROI coordinates and saves the results in out_file_name
+    """
+    # open file (ROI info have been removed)
+    df = pandas.read_csv(in_file_name, sep=':', header=None) # change 'log.txt' to your file
+
+    final_data = log_df_to_xy_df(df)
+
+    # export to csv
+    final_data.to_csv(out_file_name)
+
+    # Remove extra row from pivot things
+    with open(out_file_name, 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(out_file_name, 'w') as fout:
+        fout.writelines(data[1:])
 
 
-# open file (ROI info have been removed)
-df = pandas.read_csv('log.txt', sep=':', header=None) # change 'log.txt' to your file
+def process_roi_dir(in_dir, out_dir):
+    """
+    Process all ROI coordinate files in in_dir and save the results to out_dir
+    """
+    files = os.listdir(in_dir)
+    for f in files:
+        # check for file type
+        if ".txt" not in f:
+            continue
 
-final_data = log_df_to_xy_df(df)
+        # contruct file paths
+        in_file  = f"{in_dir}/{f}"
+        out_file = f"{out_dir}/{f.replace('.txt','.csv')}"
+        
+        # process data
+        read_roi_file_parse_and_save(in_file, out_file)
 
-# export to csv
-final_data.to_csv('data_out.csv')
 
-# Remove extra row from pivot things
-with open('data_out.csv', 'r') as fin:
-    data = fin.read().splitlines(True)
-with open('data_out.csv', 'w') as fout:
-    fout.writelines(data[1:])
+# Begining of program
+if __name__ == "__main__":
+    # handle arguments
+    arg_parser = argparse.ArgumentParser(description="Parses the coordinates of ROI into x,y columns")
+    arg_parser.add_argument('--in-file', action='store', type=str, required=False)
+    arg_parser.add_argument('--out-file', action='store', type=str, required=False)
+    arg_parser.add_argument('--in-dir', action='store', type=str, required=False)
+    arg_parser.add_argument('--out-dir', action='store', type=str, required=False)
 
+    args = arg_parser.parse_args()
+
+    # process args
+    if args.in_file and args.out_file:
+        # handle single file
+        read_roi_file_parse_and_save(args.in_file_name, args.out_file_name)
+
+    elif args.in_dir and args.out_dir:
+        # handle full directory
+        process_roi_dir(args.in_dir, args.out_dir)
+
+    else:
+        print("Invalid input")
